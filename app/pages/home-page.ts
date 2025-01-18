82d0cf6084d810a98ed3172b1804ddf15596535e
fnvrxx <fajardios01@gmail.com>
2025-01-19 01:43:59 +0700

    add photo

---
+++ app/pages/home-page.ts
@@ -0,0 +1,6 @@
+import { NavigatedData, Page } from '@nativescript/core'
+
+export function onNavigatingTo(args: NavigatedData) {
+    const page = <Page>args.object
+    page.bindingContext = {}
+}
